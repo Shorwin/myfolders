3a8485fb1ef878d32eb86bd03f7b4e3907791342
Shorwin <40027266+Shorwin@users.noreply.github.com>
2018-06-07 13:31:45 +0200

    Add files via upload
    FilesPack1

---
+++ Site Web/Projets/Shorwin/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Page D'Acceuil</title>
+		<link rel="stylesheet" href="css/styles.css">
+
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+	</head>
+	<body>
+		<!-- ENREGISTRER SOUS FORME DE :::: HEADER.PHP!-->
+		<header>
+
+
+			<!-- ENREGISTRER SOUS FORME DE :::: HEADER.PHP!-->
+
+
+
+			<nav class="menu-nav">
+				
+				<ul>
+					
+					<li class="btn"><a href="index.php"><i class="fas fa-home"></i>
+
+Acceuil</a></li>
+					<li class="btn"><a href="langages.php"> <i class="fas fa-code"></i> Langages</a></li>
+					<li class="btn"><a href="apropos.php"> <i class="fas fa-address-book"></i> À Propos</a> </li>
+					<li class="btn"><a href="contact.php"><i class="fas fa-at"></i> Contact</a></li>
+
+					<!-- ENREGISTRER SOUS FORME DE :::: HEADER.PHP!-->
+
+				</ul>
+
+			</nav>
+
+			
+		</header>
+
+	</body>
+
+</html>
+
+
+<!-- ENREGISTRER SOUS FORME DE :::: HEADER.PHP!-->
